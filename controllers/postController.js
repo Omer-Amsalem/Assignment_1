7556f9cf80cb222affceab006315303052028c0d
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-11-21 20:50:17 +0200

    create get post by id, get post by sender, update post by id API's

--- controllers/postController.js
+++ controllers/postController.js
@@ -27,4 +27,39 @@ const getPosts = asyncRest(async (req, res) => {
 });
 
 
-export { createPost, getPosts };
+const getPostById = asyncRest(async (req, res) => { 
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404);
+      throw new Error("Post not found.");
+    }
+    res.json({
+      success: true,
+      post,
+    });
+  });
+  
+  const getPostsBySender = asyncRest(async (req, res) => {
+    const posts = await Post.find({ sender: req.params.sender });
+    res.json({
+      success: true,
+      posts: posts,
+    });
+  });
+  
+  const updatePost = asyncRest(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404);
+      throw new Error("Post not found.");
+    }
+    post.message = req.body.message || post.message;
+    post.sender = req.body.sender || post.sender;
+    const updatedPost = await post.save();
+    res.json({
+      success: true,
+      post: updatedPost,
+    });
+  });
+  
+  export { createPost, getPosts, getPostById, getPostsBySender, updatePost };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createPost, getPosts} from "../controllers/postController.js"; 
+import {createPost, getPosts, getPostById, getPostsBySender, updatePost } from "../controllers/postController.js"; 
 
 const router = express.Router();
 
 router.route("/").post(createPost);
-
 router.route("/").get(getPosts);
+router.route("/:id").get(getPostById);
+router.route("/sender/:sender").get(getPostsBySender);
+router.route("/:id").put(updatePost);
+
+
 
 export default router;
